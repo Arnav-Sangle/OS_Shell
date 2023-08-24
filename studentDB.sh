5108c907fdef40ec9f81ed0f3d96f92cd6c83eb4
Arnav-Sangle <90949554+Arnav-Sangle@users.noreply.github.com>
2023-08-24 08:47:51 +0530

    created sunt DB menu, cases pending

---
+++ studentDB.sh
@@ -0,0 +1,49 @@
+clear
+while [ 1 ]
+do
+	#for \n,\t to work put string in quotes
+	echo "\nMENU
+	1. Create
+	2. Display
+	3. Insert
+	4. Search
+	5. Modify
+	6. Delete
+	7. Exit\n"
+
+	echo  "Enter choice: \c" # \c is used to take input on same line
+	read ch
+
+	case $ch in
+		1)
+		       	echo -n "Enter file name: " # -n is also used for taking input on same line
+			read fname
+			;;
+
+		2)
+			
+			;;
+		
+		3) 
+			;;
+			
+		4)
+			;;
+
+		5)	
+			;;
+
+		6)
+			;;
+
+		7)
+			exit
+			;;
+
+		*)	#Default
+			echo "Wrong choice please try again!"
+			;;
+
+	esac
+
+done
