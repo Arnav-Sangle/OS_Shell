4aa38abff6766b2250b32ca582c9c61c5169f0aa
Arnav-Sangle <90949554+Arnav-Sangle@users.noreply.github.com>
2023-08-28 11:55:24 +0530

    5 and 6 remain

--- studentDB.sh
+++ studentDB.sh
@@ -28,14 +28,62 @@ do
 			;;
 
 		2)
-			
-			;;
+                        echo -n "Enter file name: "
+                        read fname
+                        if [ -e $fname ]
+                        then
+                                echo "Content of File: "
+                                sort -n $fname   #sorts file containing numeric data in ascending order 
+
+                        else
+                                echo "File does not exist!"
+                        fi
+                        ;;
 		
 		3) 
-			;;
+  			echo -n "Enter file name: "
+                        read fname
+                        if [ -e $fname ]
+                        then
+                                echo -n "Enter roll no: "
+                                read roll
+                                grep -w "$roll" $fname          #output word containing "pattern" as a whole not as a sub-string
+                                ans=$?          # $? captures exit status of previous command, 0=success 1=fail
+                                if [ $ans -eq 0 ]
+                                then
+                                        echo "record already exists"
+                                else
+                                        echo -n "Enter name of student: "
+                                        read name
+                                        echo $roll $name >> $fname      #entering inputed record data 
+                                        echo "Record Inserted successfully"
+
+                                fi
+                        else
+                                echo "file not exists"
+                        fi
+                        ;;
 			
 		4)
-			;;
+			echo -n "Enter the file name: "
+                        read fname
+                        if [ -e $fname ]
+                        then
+                                echo -n "Enter the roll no: "
+                                read roll
+                                grep -w "$roll" $fname
+                                ans=$?
+                                if [ $ans -eq 0 ]
+                                then
+                                        echo "Record found"
+                                else
+                                        echo "Record Not found!"
+                                fi
+                        else
+                                echo "File does Not exist!"
+                        fi
+                        ;;
+
 
 		5)	
 			;;
