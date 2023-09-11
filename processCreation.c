f85e74c51ff12ed1dd8167b1ea94105a4ad1a688
Arnav-Sangle <90949554+Arnav-Sangle@users.noreply.github.com>
2023-09-11 09:23:47 +0530

    added comments

--- processCreation.c
+++ processCreation.c
@@ -1,60 +1,91 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<ctype.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/wait.h>
+/*
+Press v (lowercase) to enter visual mode. 
+This will start selecting from where your cursor is.
 
-void asc(int* , int);
-void dsc(int*, int);
+Press V (uppercase) to enter visual line mode. 
+This will select the entire line.
+
+Press CTRL+V to enter visual block mode.
+
+Move your cursor to the end of where you want to copy or cut.
+Press y to copy. 
+Press d to cut 
+
+Move the cursor to where you want to paste your selection.
+Press P (uppercase) to paste before your cursor. 
+Press p (lowercase) to paste after your cursor.
+*/
+
+/*
+ * ctrl+h backspace
+ * ctrl+j next line
+*/
+
+
+#include<stdio.h>
+#include<stdlib.h>	//malloc
+#include<ctype.h>	
+#include<unistd.h>	//getpid()=get curr process pid, getppid()=get curr processs's Parent pid 
+#include<sys/types.h> 	//for pid_t, declare process_id type variable
+#include<sys/wait.h>	//to call wait() function
+
+void asc(int*, int); 	//ascending
+void dsc(int*, int);	//descending
 
 int main() {
-	int *a, n, i;
-	pid_t pid;
-
+	//int a=0, b; is possible
+	int *a, n, i;	//a* array pointer
+	pid_t pid;	//process id variable -> pid
+		
 	printf("\nEnter the no of array elements: ");
 	scanf("%d", &n);
-	a = (int*)malloc(n*sizeof(int));
+	a = (int*)malloc(n*sizeof(int));	//assigning (int)memory to array pointer
 	
 	printf("\nEnter the array elements: ");
 	for(int i=0; i<n; i++) {
 		scanf("%d", &a[i]);
 	}
 
+	//fork() creates new child process, concurrently runs the same process written under it again 
+	//if fork() called n times,	process runs 2^n times
+	// -ve:		fail	
+	// 0: 		success return to Pchild
+	// +ve:		return to Pparent, contain pid of newly created Pchild	
 	pid = fork();
+	
 	if(pid < 0) {
-		perror("Fork error");	// perror --> error msg during process
+		perror("Fork error");	//prints custom msg before actual error msg
 	}
 	else if(pid == 0) {
-		printf("Child process id: %ld", (long)getpid());
+		printf("\nChild process id: %ld", (long)getpid()); 
 	}
 	else {
-		printf("Parent process id: %ld", (long)getpid());
+		printf("\nParent process id: %ld", (long)getpid()); //if child not created get pid of parent
 	}
 
 
 	switch(pid) {
-	
 		case -1:
 			printf("\nFork Error\n");
 			exit(-1);
 
 		case 0:
-			printf("Child executes...\n");
+			printf("\nChild executes...\n");
+			//system("ps -elf");	//ps -elf = show all process with complete info
+			asc(a,n);	//arrange array in ascending order
+			exit(pid);
 
-			//system("ps -elf");
-			asc(a,n);
-			exit(pid);
-		
 		default:
-		       wait(NULL);    //toggle for zombie process
-			printf("\nParent executes...");
-	 		dsc(a, n);
-			system("ps -elf");
+			wait(NULL);	//toggle for Zombie process (child not removed from processTable after execution)
+					//wait() execute all child process, removes Zombie
+			printf("\nParent executes...\n");
+			dsc(a,n); 	//arrange array in descending order
 			exit(pid);
-			//system("ps -elf");		
+			//system("ps -elf");
+			// https://www.xitalogy.com/linux-unix/2020/02/22/ps-elf-filter-results-effectively-and-see-if-a-linux-process-is-running.html
+			
 	}
-	
+
 }
 
 void asc(int *a, int n) {
@@ -75,8 +106,9 @@ void asc(int *a, int n) {
 		printf("%d ", a[i]);
 	}
 
-	printf("\n");
+	printf("\n\n\n");
 }
+
 
 void dsc(int *a, int n) {
         int i, j, temp;
@@ -95,5 +127,5 @@ void dsc(int *a, int n) {
                 printf("%d ", a[i]);
         }
 
-        printf("\n");
+        printf("\n\n\n");
 }
