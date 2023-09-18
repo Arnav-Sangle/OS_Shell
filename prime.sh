c6693065fa58aa00d0efb9c7c56070bdf870966f
Arnav-Sangle <90949554+Arnav-Sangle@users.noreply.github.com>
2023-09-18 07:43:40 +0530

    corrected for test case = 4

--- prime.sh
+++ prime.sh
@@ -5,7 +5,7 @@ m=`expr $n / 2`
 i=2
 #flag=0
 
-while [ $i -lt $m ]
+while [ $i -lt $m ]	#if n=4 then m=2, so use -le instead of -lt
 do
 	if [ `expr $n % $i` -eq 0 ]
 	then
@@ -19,10 +19,12 @@ do
 	fi
 done
 
+#echo $i $m
+
 if [ $m -eq 0 ] #1/2=0
 then
 	echo "Neither Prime nor Composite"
-elif [ $m -eq 1 -o $i -eq `expr $m - 1` ] #2/2=1 OR i=m-1 i.e. loop complete
+elif [ $m -eq 1 -o `expr $i - 1` -eq $m ] #2/2=1 OR i=m-1 i.e. loop complete
 then
 	echo "Prime"
 fi
